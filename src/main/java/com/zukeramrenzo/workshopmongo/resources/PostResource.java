dc49d8372616788d1bd3d8996a343aae177b8e87
Renzo Zukeram <renzo.zuk19@gmail.com>
2023-08-25 02:15:23 -0300

    Some little changes

--- src/main/java/com/zukeramrenzo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/zukeramrenzo/workshopmongo/resources/PostResource.java
@@ -5,7 +5,11 @@ import com.zukeramrenzo.workshopmongo.resources.util.URL;
 import com.zukeramrenzo.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.List;
